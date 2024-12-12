eaebb33c595a702f5bd3dec62a4be5bea7d0068a
0xBigBee <0xbigbee@protonmail.com>
2024-12-11 18:27:53 -0800

    Adding front page image

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,5 @@
 import Seo from "@components/Seo.tsx";
+import LoadAwareImage from "@islands/LoadAwareImage.tsx";
 import type { PageProps } from "$fresh/server.ts";
 
 export default function Home({ url }: PageProps) {
@@ -6,6 +7,21 @@ export default function Home({ url }: PageProps) {
     <>
       <Seo url={url.toString()} templateTitle="Home" />
       <h1 class="text-2xl">Home</h1>
+      <LoadAwareImage
+        src="/images/pages/shrew-sunvalley.jpg"
+        alt="The Taming of the Shrew"
+        width={1150}
+        height={614}
+        layout="fill"
+        objectFit="cover"
+        className="mx-auto w-full my-3"
+      />
+      <p>
+        Shrew, SunValley, 2018, Written by Amy Freed, Directed by Ben Newman.
+      </p>
+      <p>
+        Photo copyright Bruce Kroeze, 2018.
+      </p>
     </>
   );
 }
